0cc5244f631b0e09fea7c052de89b42ce7776724
K Harsh <harshnaidu2u@gmail.com>
2023-08-29 18:53:43 +0530

    after final logic and scoring

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const gameBoard = document.getElementById("gameboard");
-const info = document.getElementById("info");
+const infoDisplay = document.getElementById("info");
 const startCells = [
     "", "", "", "", "", "", "", "", ""
 ];
+
+let go = "circle";
+infoDisplay.textContent = "Circle turn first";
 
 function createBoard() {
     startCells.forEach((cell, index) => {
@@ -19,6 +22,40 @@ createBoard();
 
 function addGo(event)  {
     const goDisplay = document.createElement('div');
-    goDisplay.classList.add('cross');
+    goDisplay.classList.add(go);
     event.target.append(goDisplay);
+    go = go === "circle" ? "cross" : "circle";
+    infoDisplay.textContent = "it is now " + go + "'s turn";
+    event.target.removeEventListener("click", addGo);
+    checkScore();
+}
+
+function checkScore() {
+    const allSquares = document.querySelectorAll(".square");
+    const winningCombos = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+    ];
+    console.log(allSquares);
+
+    winningCombos.forEach(arr => {
+        const circleWins = arr.every(cell => allSquares[cell].firstChild?.classList.contains('circle'));
+
+        if(circleWins){
+            infoDisplay.textContent = "Circle Wins!!";
+            allSquares.forEach(sq => sq.replaceWith(sq.cloneNode(true)))
+            return
+        }
+    })
+
+    winningCombos.forEach(arr => {
+        const crossWins = arr.every(cell => allSquares[cell].firstChild?.classList.contains('cross'));
+
+        if(crossWins){
+            infoDisplay.textContent = "Cross Wins!!";
+            allSquares.forEach(sq => sq.replaceWith(sq.cloneNode(true)))
+            return
+        }
+    })
 }
