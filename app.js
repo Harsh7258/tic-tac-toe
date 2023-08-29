07a6fcc7ff1d05ee172d50aa1f3b6c4e2354cab1
K Harsh <harshnaidu2u@gmail.com>
2023-08-29 18:09:14 +0530

    after appyling circle and cross on click

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
-const gameBoard = document.getElementById("#gameboard");
-const info = document.getElementById("#info");
+const gameBoard = document.getElementById("gameboard");
+const info = document.getElementById("info");
+const startCells = [
+    "", "", "", "", "", "", "", "", ""
+];
+
+function createBoard() {
+    startCells.forEach((cell, index) => {
+        const cellElement = document.createElement('div');
+        cellElement.classList.add('square');
+        cellElement.id = index;
+        gameBoard.append(cellElement);
+
+        cellElement.addEventListener('click', addGo)
+    });
+};
+
+createBoard();
+
+function addGo(event)  {
+    const goDisplay = document.createElement('div');
+    goDisplay.classList.add('cross');
+    event.target.append(goDisplay);
+}
